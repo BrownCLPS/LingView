72144d166655e248386b21954799a73362772c12
Eli Sharf <esharf@google.com>
2020-11-19 23:23:13 -0800

    cleaning up

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -1,37 +1,15 @@
 import React from 'react';
 
-function renderEn() {
-  return (
-    <p> Hi! </p>
-  );
-}
-
-function renderFr() {
-  return (
-    <p> Bonjour! </p>
-  );
-}
-
-function renderSp() {
-  return (
-    <p> Hola! </p>
-  );
-}
+const aboutPageText = {
+  "en" : <p>Hi!</p>,
+  "fr" : <p>Bonjour!</p>,
+  "sp" : <p>Hola!</p>,
+};
 
 export function AboutPage({ lang }) {
-  let text;
-  if (lang === "en") {
-    text = renderEn();
-  }
-  else if (lang == "fr") {
-    text = renderFr();
-  }
-  else if (lang == "sp") {
-    text = renderSp();
-  }
   return (
       <div style={{margin: "0.25in"}}>
-      {text}
+      {aboutPageText[lang]}
       </div>
   );
 }
