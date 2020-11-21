ac6da58c209233768815fb91724a80a49c337f8c
Eli Sharf <esharf@google.com>
2020-11-21 10:53:21 -0800

    Remove dependencies in App.jsx

--- jsx/App/App.jsx
+++ jsx/App/App.jsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
 // // import id from 'shortid';
-import { Route } from 'react-router-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import { AboutPage } from './AboutPage.jsx';
 import { Footer } from './Footer.jsx';
-import { LandingPage } from './LandingPage.jsx';
-import { GlossaryPage } from './GlossaryPage.jsx';
 import { Header } from './Header.jsx';
-import { StoryIndex } from './StoryIndex.jsx';
-import { Search } from './Search.jsx';
-import { Stories } from './Stories/Stories.jsx';
 
 
 export function App({ data }) {
