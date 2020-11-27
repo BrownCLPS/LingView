bbe9786e3eed40d5d423cc035a731be1a705a69c
Miriam Ashton <miriamashton3@gmail.com>
2020-11-27 15:57:00 -0500

    Selector styling

--- jsx/App/locale/LocaleSelect.jsx
+++ jsx/App/locale/LocaleSelect.jsx
@@ -11,10 +11,10 @@ export const LocaleSelect = () => {
   const { locale, setLocale } = useContext(LocaleContext);
   const handleSelect = e => setLocale(e.target.value);
   return (
-      <select className="localeSelector" onChange={handleSelect} value={locale}>
-        <option value={LocaleConstants.ENGLISH}>English</option>
-        <option value={LocaleConstants.ESPANOL}>Español</option>
-        <option value={LocaleConstants.FRANCAIS}>Français</option>
-      </select>
+    <select name="locale" id="locale-select" className="localeSelector" onChange={handleSelect} value={locale}>
+      <option value={LocaleConstants.ENGLISH}>English</option>
+      <option value={LocaleConstants.ESPANOL}>Español</option>
+      <option value={LocaleConstants.FRANCAIS}>Français</option>
+    </select>
   );
 }
