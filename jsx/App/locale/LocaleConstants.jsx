d3689407dee0c9291585e97dffc7981642db0a4d
Eli Sharf <esharf@google.com>
2020-11-27 23:23:08 -0800

    Move JSX for full pages into their respective files. Also fix bug with audio bar.

--- jsx/App/locale/LocaleConstants.jsx
+++ jsx/App/locale/LocaleConstants.jsx
@@ -1,4 +1,7 @@
 // Put language constants here to use in your display.
+// JSX for entire pages, specifically the Landing page, Glossary page, and the
+// About page, are in the respective files themselves
+// (e.g. jsx/App/LandingPage.jsx).
 export const ENGLISH = "en";
 export const ESPANOL = "es";
 export const FRANCAIS = "fr";
@@ -6,8 +9,8 @@ export const FRANCAIS = "fr";
 // Put default language here.
 export const DEFAULT_LOCALE = ENGLISH;
 
-// Below, write the text or JSX that goes in various parts of the website for
-// each language.
+// Below, write the text that goes in various parts of the website for each
+// language.
 
 // NavBar text (header).
 export const navBarTitleText = {
@@ -38,66 +41,6 @@ export const navBarIndexText = {
   [ENGLISH]: "Index of Texts",
   [ESPANOL]: "Índice de textos",
   [FRANCAIS]: "Index des Textes",
-};
-
-// About page, can be formatted however one pleases using JSX.
-
-export const aboutPageJSX = {
-  [ENGLISH]:
-    <div>
-      <p>Input content here :) </p>
-      <p>To customize this welcome text, edit the jsx/App/LocaleConstants.jsx file, then run webpack for your changes to take effect. </p>
-    </div>,
-  [ESPANOL]:
-    <div>
-      <p>Ingrese el contenido aquí :) </p>
-      <p>Para personalizar este texto de bienvenida, edite el archivo 'jsx/App/LocaleConstants.jsx' y luego ejecute el paquete web para que los cambios surtan efecto. </p>
-    </div>,
-  [FRANCAIS]:
-    <div>
-      <p>Saisissez le contenu ici :) </p>
-      <p>Pour personnaliser ce texte de bienvenue, modifiez le fichier 'jsx/App/LocaleConstants.jsx', puis exécutez webpack pour que vos modifications prennent effet. </p>
-    </div>,
-};
-
-// Glossary page, can be formatted however one pleases using JSX.
-
-export const glossaryPageJSX = {
-  [ENGLISH] :
-    <div>
-      <p>Input glossary here :) </p>
-      <p>To customize this welcome text, edit the jsx/App/LocaleConstants.jsx file, then run webpack for your changes to take effect. </p>
-    </div>,
-  [ESPANOL] :
-    <div>
-      <p>Ingrese el glosario aquí :) </p>
-      <p>Para personalizar este texto de bienvenida, edite el archivo 'jsx/App/LocaleConstants.jsx' y luego ejecute el paquete web para que los cambios surtan efecto. </p>
-    </div>,
-  [FRANCAIS] :
-    <div>
-      <p>Saisissez le glossaire ici :) </p>
-      <p>Pour personnaliser ce texte de bienvenue, modifiez le fichier 'jsx/App/LocaleConstants.jsx', puis exécutez webpack pour que vos modifications prennent effet. </p>
-    </div>,
-};
-
-// Landing page, can be formatted however one pleases using JSX.
-
-export const landingPageJSX = {
-  [ENGLISH]:
-    <div>
-      <p>Welcome! This website is powered by LingView. Click <a href='#/index'>"Index of Texts"</a> to see some example texts.</p>
-      <p>To customize this welcome text, edit the jsx/App/LocaleConstants.jsx file, then run webpack for your changes to take effect. </p>
-    </div>,
-  [ESPANOL]:
-    <div>
-      <p>¡Bienvenido! Este sitio web funciona con LingView. Haga clic <a href='#/index'>"Índice de textos"</a> para ver algunos textos de ejemplo. </p>
-      <p>Para personalizar este texto de bienvenida, edite el archivo 'jsx/App/LocaleConstants.jsx' y luego ejecute el paquete web para que los cambios surtan efecto. </p>
-    </div>,
-  [FRANCAIS]:
-    <div>
-      <p>Bienvenue! Ce site Web est alimenté par LingView. Cliquez <a href='#/index'>"Index des Textes"</a> pour voir quelques exemples de textes. </p>
-      <p>Pour personnaliser ce texte de bienvenue, modifiez le fichier 'jsx/App/LocaleConstants.jsx', puis exécutez webpack pour que vos modifications prennent effet. </p>
-    </div>,
 };
 
 // Search page text
